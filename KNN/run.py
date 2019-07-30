c7b466adb2238139a8ecd46e14603fec5054b858
mMove <378638180@qq.com>
2019-07-30 21:17:42 +0800

    classify

---
+++ KNN/run.py
@@ -0,0 +1,4 @@
+from KNN import kNN
+
+group, labels= kNN.createDataSet()
+a= kNN.classify0([0, 0], group, labels, 3)
--- run.py
+++
@@ -1,3 +0,0 @@
-import kNN
-group, labels=kNN.createDataSet()
-a=kNN.classify0([0,0], group, labels, 3)
